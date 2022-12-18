7b749e97c644fddc97cd4604274ee86849efdd3a
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-12-18 15:44:50 +0000

    Fix profiles model error

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Models):
+class UserProfile(models.Model):
     """
     A model for users
     """
